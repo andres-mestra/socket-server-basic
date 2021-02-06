31726ea823bf64b6de9185e6c242ba779e4df623
Andres Mestra <andresmestra11a@gmail.com>
2021-02-06 15:03:46 -0500

    Refactorización servidor

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-const express = require('express')
+const Server = require('./models/server');
 
-//Serividor express
-const app = express()
-const PORT = process.env.PORT || 3000;
+const server = new Server();
 
-//Servidor de sockets
-const server = require('http').createServer(app);
-
-//Configuración del socket server
-const io = require('socket.io')(server);
-
-//Desplegar directorio publico
-app.use( express.static(__dirname + '/public') )
-
-io.on('connection', ( socket ) => { 
-  
-  socket.on('mensaje-to-server', (data) => {
-    console.log(data)
-    
-    //Emit a todos los clientes conectados en mismo namespace
-    io.emit('mensaje-from-server', data );
-  });
-
-});
-
-
-
-
-server.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}`)
-});
+server.execute();
---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io');
+const path = require('path');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+
+    //Http server
+    this.server = http.createServer(this.app)
+
+    //Configuración de socket
+    this.io = socketIo(this.server, { /* Configuraciones */ })
+  }
+
+  middlewares() {
+    //Desplegar directorio publico
+    this.app.use(express.static( path.resolve(__dirname, '../public') ))
+  }
+  
+
+  configSockets() {
+
+  }
+
+
+  execute() {
+    //Inicializa middlewares
+    this.middlewares()
+
+    //Inicializar sockets
+    this.configSockets()
+
+    //Inicializar server
+    this.server.listen(this.port, () => {
+      console.log(`🚀 Server ready at http://localhost:${this.port}`)
+    })
+  }
+
+
+}
+
+module.exports = Server;
